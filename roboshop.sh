8a192f74cc3665e11701d3b01c8db58330766d1f
Haima <seethasri001@gmail.com>
2025-05-26 17:00:47 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-07c50cacebb7651f8"
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "car" "shipping" "payment" "dispatch" "frontend")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
 ZONE_ID="Z07217861L8HXB4SGRF90"
 DOMAIN_NAME="haima.site"
@@ -17,4 +17,22 @@ else
 IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 fi
 echo "$INSTANCE Ip address: $IP"
+
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
 done
